f1261bbb365f58ae134249f1c7af6a76e90c78e1
NullVoxPopuli <LPSego3+dev@gmail.com>
2019-10-04 14:09:09 -0400

    include the language server in the npm package

--- src/index.ts
+++ src/index.ts
@@ -12,11 +12,7 @@ import {
   WorkspaceConfiguration,
 } from 'coc.nvim';
 
-const languageServerPath = [
-  'node_modules',
-  '@emberwatch',
-  'ember-language-server',
-];
+const languageServerPath = ['lib', 'ember-language-server'];
 const serverBin = ['lib', 'start-server.js'];
 
 export async function activate(context: ExtensionContext): Promise<void> {
@@ -36,14 +32,6 @@ export async function activate(context: ExtensionContext): Promise<void> {
   let binPath = context.asAbsolutePath(
     path.join(...languageServerPath, ...serverBin)
   );
-
-  let repoPath = context.asAbsolutePath(path.join(...languageServerPath));
-
-  console.error(repoPath);
-  let cocEmberPath = context.asAbsolutePath(path.join('.'));
-  console.error(cocEmberPath);
-
-  await checkRequirements(repoPath, binPath, cocEmberPath);
 
   let debugOptions = isDebugging
     ? { execArgv: ['--nolazy', '--inspect=6004'] }
@@ -110,42 +98,4 @@ async function isEmberCliProject(): Promise<boolean> {
   let emberCliBuildFile = await workspace.findUp('ember-cli-build.js');
 
   return !!emberCliBuildFile;
-}
-
-async function checkRequirements(
-  repoPath: string,
-  binPath: string,
-  cocEmberPath: string
-): Promise<void> {
-  let isAlreadyBuilt = doesFileExist(binPath);
-
-  console.error('isAlreadyBuilt', isAlreadyBuilt);
-
-  if (isAlreadyBuilt) {
-    return;
-  }
-
-  // Ensure we've fetched our own dependcies.
-  // coc-nvim does npm install on coc-* packages.
-  // npm install applies the .npmignore to git repos
-  // which means if your repo does not include the built
-  // files, there is no way to reference something from git.
-  //
-  // so... we need to install our own dependencies....
-  await execa.command(`npm install`, { cwd: cocEmberPath });
-
-  // running yarn on the ember-language-server will also build it
-  await execa.command(`npm run coc:prepare`, {
-    cwd: repoPath,
-  });
-}
-
-function doesFileExist(filePath: string): boolean {
-  try {
-    const result = execa.commandSync(`test -f ${filePath}`);
-
-    return result.exitCode === 0;
-  } catch (e) {
-    return false;
-  }
 }
