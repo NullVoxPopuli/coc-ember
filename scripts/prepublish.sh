c0f0f281c4db9b196bfc6c9318e1011c7397c41e
NullVoxPopuli <LPSego3+dev@gmail.com>
2021-01-17 16:48:12 -0500

    feat: use published @lifeart/ember-language-servers package

--- scripts/prepublish.sh
+++ scripts/prepublish.sh
@@ -7,8 +7,5 @@ yarn clean
 echo "Building coc-ember..."
 yarn build:js
 
-echo "Building and Bundling the unstable/experimental Ember Language Server..."
-yarn build:language-server
-
 echo "Bundling UELS addons..."
 yarn build:addons
